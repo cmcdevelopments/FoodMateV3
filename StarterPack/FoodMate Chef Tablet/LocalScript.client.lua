13222cf0ce180eba204f09cdfbc623ab427b4ec0
Newt <55791489+Trisxcal@users.noreply.github.com>
2022-08-09 21:52:37 +0200

    Delete StarterPack directory

--- StarterPack/FoodMate Chef Tablet/LocalScript.client.lua
+++
@@ -1,7 +0,0 @@
-script.Parent.Equipped:Connect(function()
-	game.Players.LocalPlayer.PlayerGui:WaitForChild('foodMateChef').Enabled = true
-end)
-
-script.Parent.Unequipped:Connect(function()
-	game.Players.LocalPlayer.PlayerGui:WaitForChild('foodMateChef').Enabled = false
-end)
